0674a1feaa1343b3608e35c76ceff8f1bb288a96
sjho482 <sjho482@naver.com>
2021-09-13 13:07:00 +0900

    practice.java.cal

---
+++ src/calender/calender.java
@@ -0,0 +1,41 @@
+package calender;
+
+public class calender {
+	private final int [] MAX_DAYS={31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	private final int [] LEAPMAX_DAYS={31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	public boolean isleapyear(int year) {
+		if (year % 4 ==0 && (year % 100 != 0 || year % 400 ==0))
+			return true ;
+		else
+	return false;	
+	}
+	
+	public int maxdaysofmonth(int year, int month) {
+		  if(isleapyear(year)) {
+		    	return LEAPMAX_DAYS[month-1];
+		    }else {
+		return MAX_DAYS[month-1];
+	}
+	}
+	
+	
+	public void printcalender2(int year ,int month) {
+	System.out.printf("     <<%4d %3d>>\n" ,year, month);
+    System.out.println(" 일  월  화  수 목 금 토");  
+    int maxdays = maxdaysofmonth(year, month);
+    for(int i = 1; i <=maxdays; i++) {
+    	System.out.printf(   "%3d",i);
+    	if(i % 7 == 0) 
+    		System.out.println();
+    }
+	
+    System.out.println();
+//	System.out.println(" 1  2  3  4  5  6  7");
+//	System.out.println(" 8  9 10 11 12 13 14");
+//	System.out.println("15 16 17 18 19 20 21");
+//	System.out.println("22 23 24 25 26 27 28");
+	}
+	public static void main(String[] args) {
+	
+	}
+	}
---
+++ src/calender/calender2.java
@@ -0,0 +1,31 @@
+package calender;
+
+import java.util.Scanner;
+
+public class calender2 {
+
+	public static void main(String[] args) {
+
+	Scanner scanner= new Scanner(System.in);
+	calender cal= new calender();
+	while (true){
+	System.out.println("년도를 입력하세요.");
+	System.out.print("Year>");
+	int year = scanner.nextInt();
+	System.out.println("달을 입력하세요");
+	System.out.print("Month>");
+	int days = scanner.nextInt();
+	if (days == -1) {
+				break;
+	}
+	if (days > 12 ) { 
+		break;
+	}
+	cal.printcalender2(year, days);
+	}
+
+	scanner.close();
+
+}
+}
+
