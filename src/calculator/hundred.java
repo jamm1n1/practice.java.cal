b2d114fcd86ddadc0b5ff0a4c9341ac25236bc65
sjho482 <sjho482@naver.com>
2021-10-12 14:13:24 +0900

    1~100

--- src/calculator/hundred.java
+++ src/calculator/hundred.java
@@ -7,11 +7,14 @@ public class hundred {
 		int n = 0;
 		for(int i = 1; i<=h; i++) {
 			n+=i;
-				
+			
+			
 			
 			
 		}
+		System.out.println("1부터100까지의합은");
 		System.out.println((n+n)/2);
+	
 	}
 
 }
