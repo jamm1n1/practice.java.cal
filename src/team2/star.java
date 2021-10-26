d73539b8f1e3276ebee3d5a5afd0dfc7a0c6909a
sjho482 <sjho482@naver.com>
2021-10-26 12:09:12 +0900

    fail

---
+++ src/team2/star.java
@@ -0,0 +1,18 @@
+package team2;
+
+public class star {
+
+	public static void main(String[] args) {
+		
+		for(int i = 0; i<5; i++) {
+			for(int j =0; j<4-i; j++) {
+				System.out.print("");
+			}
+			for(int j = 5; j>i; j--) {
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+	}
+
+}
